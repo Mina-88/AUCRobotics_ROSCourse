78128183c3b581d826d3fee13d13a3733ab31859
MinaWM <minawmikhael@aucegypt.edu>
2020-08-14 17:19:23 -0500

    the first two days

---
+++ day1/Task:Running_code_in_terminal/src/build/CMakeFiles/nameCppCmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nameCppCmake.dir/name.cpp.o"
+  "nameCppCmake.pdb"
+  "nameCppCmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nameCppCmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ day1/Task:Running_code_in_terminal/src/name.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "I'm a software engineer, now learning ros" << endl;
+    return 0;
+}
