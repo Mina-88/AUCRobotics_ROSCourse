2945c635046ed2258a9598a2efc183ff4e30baf4
MinaWM <minawmikhael@aucegypt.edu>
2020-08-15 08:34:37 -0500

    day3 task2

---
+++ day3/task2/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mina/Desktop/AUCRobotics_ROSCourse/AUCRobotics_ROSCourse/day3/task2/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/mina/Desktop/AUCRobotics_ROSCourse/AUCRobotics_ROSCourse/day3/task2/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/mina/Desktop/AUCRobotics_ROSCourse/AUCRobotics_ROSCourse/day3/task2/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/mina/Desktop/AUCRobotics_ROSCourse/AUCRobotics_ROSCourse/day3/task2/src:$ROS_PACKAGE_PATH"
