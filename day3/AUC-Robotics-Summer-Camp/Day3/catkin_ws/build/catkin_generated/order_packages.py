50fac42c9712fd657bdc4858fb29d46f85037074
MinaWM <minawmikhael@aucegypt.edu>
2020-08-15 12:40:41 -0500

    running_the_cloned_package

---
+++ day3/AUC-Robotics-Summer-Camp/Day3/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/mina/Desktop/AUCRobotics_ROSCourse/AUCRobotics_ROSCourse/day3/AUC-Robotics-Summer-Camp/Day3/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/mina/Desktop/AUCRobotics_ROSCourse/AUCRobotics_ROSCourse/day3/mina_ws/devel;/opt/ros/melodic'.split(';') if '/home/mina/Desktop/AUCRobotics_ROSCourse/AUCRobotics_ROSCourse/day3/mina_ws/devel;/opt/ros/melodic' != '' else []
---
+++ day3/AUC-Robotics-Summer-Camp/Day3/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mina/Desktop/AUCRobotics_ROSCourse/AUCRobotics_ROSCourse/day3/AUC-Robotics-Summer-Camp/Day3/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/mina/Desktop/AUCRobotics_ROSCourse/AUCRobotics_ROSCourse/day3/AUC-Robotics-Summer-Camp/Day3/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mina/Desktop/AUCRobotics_ROSCourse/AUCRobotics_ROSCourse/day3/AUC-Robotics-Summer-Camp/Day3/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/mina/Desktop/AUCRobotics_ROSCourse/AUCRobotics_ROSCourse/day3/AUC-Robotics-Summer-Camp/Day3/catkin_ws/src:$ROS_PACKAGE_PATH"
